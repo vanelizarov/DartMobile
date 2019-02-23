cce2090dc83d84add96c1e5ce05fa85ab6396112
vanelizarov <elizarov.vanya@gmail.com>
2019-02-23 20:47:42 +0300

    Initial Commit

---
+++ DartMobile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DartMobile
+//
+//  Created by vanya elizarov on 23/02/2019.
+//  Copyright © 2019 vanya elizarov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
